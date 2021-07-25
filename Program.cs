21a7e06013b6adee3202273169e9c754fca8c548
saurabhs <saurabhpshirsath@gmail.com>
2021-07-25 19:27:13 +0530

    [saurabh] Add; Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison!");
+        }
+    }
+}
